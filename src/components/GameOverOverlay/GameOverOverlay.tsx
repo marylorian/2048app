490e42321281a721474bd0f8e2e45eb7a8a62688
marylorian <marialobareva97@gmail.com>
2026-05-18 14:27:54 +0200

    adds eas artifacts

--- src/components/GameOverOverlay/GameOverOverlay.tsx
+++ src/components/GameOverOverlay/GameOverOverlay.tsx
@@ -26,7 +26,6 @@ export function GameOverOverlay({ onRestart }: GameOverOverlayProps) {
       style={[
         styles.overlay,
         {
-          opacity: overlayProgressRef.current,
           transform: [
             {
               scale: overlayProgressRef.current.interpolate({
